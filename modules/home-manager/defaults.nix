215fcd77cbd2aac9ac090211efe922b0605e56b5
Connor Alecks <git@econnah.uk>
2025-11-18 23:48:57 +0100

    added oil and diffview to nvim

--- modules/home-manager/defaults.nix
+++ modules/home-manager/defaults.nix
@@ -1,7 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./fish.nix
   ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      diffview-nvim
+      neogit
+      {
+        plugin = oil-nvim;
+        config = ''
+          packadd! oil.nvim
+          lua << EOF
+require("oil").setup()
+EOF
+        '';
+      }
+    ];
+  };
 }
