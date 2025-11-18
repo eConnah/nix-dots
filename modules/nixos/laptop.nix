7214d5409b4779538caca94d9b9a0723bb334fae
Connor Alecks <git@econnah.uk>
2025-11-18 21:26:36 +0100

    removed iwd backend caused too many issues and provided no noticable benefit

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -7,7 +7,5 @@
   services.logind.settings.Login.HandleLidSwitch = "suspend";
   services.power-profiles-daemon.enable = true;
   hardware.bluetooth.enable = true;
-  networking.wireless.iwd.enable = true;
-  networking.networkmanager.wifi.backend = "iwd";
 }
 
