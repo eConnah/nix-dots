f98e437c1efcd708331656c2ec5f94839fd77788
Connor Alecks <git@econnah.uk>
2025-11-20 17:29:47 +0100

    added libreoffice to laptops

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -1,10 +1,14 @@
 # General laptop settings
 
-{ ... }:
+{ pkgs, ... }:
 
 {
   powerManagement.enable = true;
   services.logind.settings.Login.HandleLidSwitch = "suspend";
   services.power-profiles-daemon.enable = true;
   hardware.bluetooth.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    libreoffice
+  ];
 }
