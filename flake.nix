c979e4b2f22e7e2e2363193b5bf61b46d86ec60c
Connor Alecks <git@econnah.uk>
2025-11-26 22:02:08 +0100

    updated flake.lock

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
