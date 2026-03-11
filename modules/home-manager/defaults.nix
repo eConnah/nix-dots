2bf7690755b701fcaa0f512b846d59769731dd04
Connor Alecks <git@econnah.uk>
2026-03-11 11:42:34 +0100

    nvim support

--- modules/home-manager/defaults.nix
+++ modules/home-manager/defaults.nix
@@ -47,6 +47,37 @@
     setlocal shiftwidth=2
     setlocal softtabstop=2
   '';
+  
+  # Detection: Tell Neovim that .rtl files are the "rtl" filetype
+  home.file.".config/nvim/ftdetect/rtl.vim".text = ''
+    autocmd BufRead,BufNewFile *.rtl set filetype=rtl
+  '';
+
+  # Syntax: The actual highlighting rules
+  home.file.".config/nvim/syntax/rtl.vim".text = ''
+    if exists("b:current_syntax")
+      finish
+    endif
+
+    syntax match rtlComment /{[^}]*}/
+    syntax match rtlRegister /\b\(IP\|IR\|SigmaA\|SigmaR\|SigmaW\|CC\|RA\|RB\|ALU\|RAM\)\b/
+    syntax match rtlBus /(\(Abus\|Bbus\|Cbus\))/
+    syntax match rtlOperator /<-\|+\|\[\|\]/
+    syntax match rtlHex /0x[0-9A-Fa-f]\+/
+
+    highlight default link rtlComment Comment
+    highlight default link rtlRegister Function
+    highlight default link rtlBus Type
+    highlight default link rtlOperator Operator
+    highlight default link rtlHex Constant
+
+    " Custom Catppuccin-matching neon overrides
+    highlight rtlHex guifg=#a6e3a1 gui=bold
+    highlight rtlBus guifg=#fab387 gui=bold
+    highlight rtlOperator guifg=#f5c2e7
+
+    let b:current_syntax = "rtl"
+  '';
 
   programs.eza = {
     enable = true;
