bea9d9cc3607b4af02641ee157d92e6175ef1def
Connor Alecks <git@econnah.uk>
2026-06-05 21:34:22 +0200

    feat(packages): install obs-studio

--- nix/hosts/onyx/default.nix
+++ nix/hosts/onyx/default.nix
@@ -45,6 +45,19 @@
             spotify
           ];
 
+          programs.obs-studio = {
+            enable = true;
+
+            package = (
+              pkgs.obs-studio.override {
+                cudaSupport = true;
+              }
+            );
+            plugins = with pkgs.obs-studio-plugins; [
+              obs-pipewire-audio-capture
+            ];
+          };
+
           theme.wallpaper = "86-01.png";
 
           home.file.".config/lazyspotify/config.yml".text = ''
