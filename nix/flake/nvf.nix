1733fe5d05e81464894703da3cba6833749c596c
Connor Alecks <git@econnah.uk>
2026-07-02 01:36:11 +0200

    feat: finish nvfModules

--- nix/flake/nvf.nix
+++ nix/flake/nvf.nix
@@ -1,8 +1,36 @@
-{ lib, ... }:
 {
-  options.flake.nvfModules = lib.mkOption {
-    type = lib.types.lazyAttrsOf lib.types.anything;
-    default = { };
-    description = "custom dendritic nvf module namespace";
+  lib,
+  flake-parts-lib,
+  moduleLocation,
+  ...
+}:
+let
+  inherit (lib)
+    mapAttrs
+    mkOption
+    types
+    ;
+in
+{
+  options = {
+    flake = flake-parts-lib.mkSubmoduleOptions {
+      nvfModules = mkOption {
+        type = types.lazyAttrsOf types.deferredModule;
+        default = { };
+        apply = mapAttrs (
+          k: v: {
+            _class = "nvf";
+            _file = "${toString moduleLocation}#nvfModules.${k}";
+            imports = [ v ];
+          }
+        );
+        description = ''
+          NVF configuration modules.
+
+          You may use this for reusable pieces of Neovim configuration, plugins,
+          and themes so that they can be referenced across different host configurations.
+        '';
+      };
+    };
   };
 }
--- nix/nvim/packages.nix
+++ nix/nvim/packages.nix
@@ -4,18 +4,18 @@
   ...
 }:
 {
-  perSystem =
-    { pkgs, ... }:
-    {
-      packages.nvim-qwerty =
+  perSystem = { pkgs, ... }: {
+    packages = {
+      nvim-qwerty =
         (inputs.nvf.lib.neovimConfiguration {
           inherit pkgs;
           modules = [
-            (self.nvfModules.defaults { inherit pkgs; })
+            self.nvfModules.defaults
             self.nvfModules.notes
             self.nvfModules.ui
             self.nvfModules.workflow
           ];
         }).neovim;
     };
+  };
 }
